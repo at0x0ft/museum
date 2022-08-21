873f3e6870c9b89c55fc0064daba42fe930ca460
at0x0ft <26642966+at0x0ft@users.noreply.github.com>
2022-08-22 01:35:54 +0900

    Add evaluator/equals.go.

---
+++ src/yaml_evaluator/evaluator/equals.go
@@ -0,0 +1,34 @@
+package evaluator
+
+import (
+    "fmt"
+    "strconv"
+    "gopkg.in/yaml.v3"
+)
+
+func IsEqualsTaggedNode(node *yaml.Node) bool {
+    rootNodeIsSubstitutionTagged := node.Kind == yaml.SequenceNode && node.Tag == "!Equals"
+    hasTwoChildNodes := len(node.Content) == 2
+    if !rootNodeIsSubstitutionTagged || !hasTwoChildNodes {
+        return false
+    }
+
+    firstChildNode := node.Content[0]
+    secondChildNode := node.Content[1]
+    firstChildIsString := firstChildNode.Kind == yaml.ScalarNode && firstChildNode.Tag == "!!str"
+    secondChildIsString := secondChildNode.Kind == yaml.ScalarNode && secondChildNode.Tag == "!!str"
+    return firstChildIsString && secondChildIsString
+}
+
+func EvaluateEquals(node *yaml.Node) error {
+    if !IsEqualsTaggedNode(node) {
+        return nil
+    }
+
+    result := strconv.FormatBool(node.Content[0].Value == node.Content[1].Value)
+    var newNodeStyle yaml.Style
+    var newNodeContent []*yaml.Node
+    fmt.Println(result)
+    node.Kind, node.Style, node.Tag, node.Value, node.Content = yaml.ScalarNode, newNodeStyle, "!!str", result, newNodeContent
+    return nil
+}
