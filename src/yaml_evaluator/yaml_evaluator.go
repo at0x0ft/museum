ddaaa8151e13a35517ee4c13df8b40aba24800e9
at0x0ft <26642966+at0x0ft@users.noreply.github.com>
2022-08-23 23:40:51 +0900

    Add first prototype.

--- src/yaml_evaluator/yaml_evaluator.go
+++ src/yaml_evaluator/yaml_evaluator.go
@@ -8,13 +8,17 @@ import (
     "gopkg.in/yaml.v3"
     "github.com/at0x0ft/cod2e2/yaml_evaluator/evaluator"
     "github.com/at0x0ft/cod2e2/yaml_evaluator/traverse"
+    "github.com/at0x0ft/cod2e2/yaml_evaluator/variable"
     // "github.com/at0x0ft/cod2e2/yaml_evaluator/debug"
 )
 
-type Config struct {
+type YamlFormat struct {
     Version string `yaml:"version"`
-    Variables map[string]map[string]string `yaml:"variables"`
-    VSCodeDevcontainer yaml.Node `yaml:"configs"."vscode_devcontainer"`
+    Variables yaml.Node `yaml:"variables"`
+    Configs struct {
+        VSCodeDevcontainer yaml.Node `yaml:"vscode_devcontainer"`
+        DockerCompose yaml.Node `yaml:"docker_compose"`
+    } `yaml:"configs"`
 }
 
 func main() {
@@ -24,28 +28,39 @@ func main() {
         return
     }
 
-    var data *Config
-    err = yaml.Unmarshal(buf, &data)
-    if err != nil {
+    var data *YamlFormat
+    if err := yaml.Unmarshal(buf, &data); err != nil {
         fmt.Println(err)
         return
     }
 
-    ch := make(chan *yaml.Node)
-    go traverse.Traverse(&data.VSCodeDevcontainer, ch, traverse.PostOrder)
-    for node := range ch {
-        err = evaluator.EvaluateVariable(node, &data.Variables)
-        if err != nil {
-            fmt.Println(err)
-            return
-        }
+    variables := variable.Parse(&data.Variables)
+    if err := evaluateYaml(&data.Configs.VSCodeDevcontainer, variables); err != nil {
+        fmt.Println(err)
+        return
+    }
+    if err := evaluateYaml(&data.Configs.DockerCompose, variables); err != nil {
+        fmt.Println(err)
+        return
     }
 
     var b bytes.Buffer
     yamlEncoder := yaml.NewEncoder(&b)
     defer yamlEncoder.Close()
     yamlEncoder.SetIndent(2)
-    yamlEncoder.Encode(&data.VSCodeDevcontainer)
+    // yamlEncoder.Encode(&data.Configs.VSCodeDevcontainer)
+    yamlEncoder.Encode(&data.Configs.DockerCompose)
 
     fmt.Print(string(b.Bytes()))
+}
+
+func evaluateYaml(rootNode *yaml.Node, variables *map[string]string) error {
+    ch := make(chan traverse.NodeInfo)
+    go traverse.Traverse(rootNode, ch, traverse.PostOrder)
+    for nodeInfo := range ch {
+        if err := evaluator.EvaluateAll(nodeInfo.Node, variables); err != nil {
+            return err
+        }
+    }
+    return nil
 }
