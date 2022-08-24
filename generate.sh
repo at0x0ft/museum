6f3ee5faaac34078e0ddbfa01808a8255f574ec6
at0x0ft <26642966+at0x0ft@users.noreply.github.com>
2022-08-25 04:40:57 +0900

    Fix generate.sh rough implementation.

--- generate.sh
+++ generate.sh
@@ -38,6 +38,7 @@ readlinkf() {
 }
 SCRIPT_PATH="$(readlinkf "${0}")"
 SCRIPT_ROOT="$(dirname -- "${SCRIPT_PATH}")"
+DEVCONTAINER_DIRNAME='.devcontainer'
 
 make_devcontainer_directory_if_not_exists() {
   if [ ! -d "${1}" ]; then
@@ -53,29 +54,48 @@ yq() {
   return 0
 }
 
-generate_devcontainer_json() {
-  local readonly YQ_DEVCONTAINER_JSON_PATH='.configs.vscode_devcontainer'
-  local readonly YQ_JSON_INDENTATION_SPACES='4'
+evaluate_yaml() {
+  local readonly DOCKER_COMPOSE_PATH="${SCRIPT_ROOT}/docker-compose.yml"
+  local readonly YAML_EVALUATOR_SERVICE_NAME='yaml_evaluator'
+  local readonly YAML_EVALUATOR_SERVICE_WORKING_DIRPATH='/workspace'
+  local readonly config_yaml_relpath="./$(basename -- "${1}")"
+  local readonly YAML_OUTPUT_RELPATH='.'
 
-  yq -o=json -I="${YQ_JSON_INDENTATION_SPACES}" "${YQ_DEVCONTAINER_JSON_PATH}" "${1}" >"${2}"
+  cp "${1}" "${2}"
+
+  docker-compose -f "${DOCKER_COMPOSE_PATH}" run --rm \
+    --user="$(id -u):$(id -g)" \
+    -v "${2}:${YAML_EVALUATOR_SERVICE_WORKING_DIRPATH}" \
+    "${YAML_EVALUATOR_SERVICE_NAME}" "${config_yaml_relpath}" "${YAML_OUTPUT_RELPATH}"
+
+  return 0
+}
+
+convert_devcontainer_yaml_to_json() {
+  local readonly YQ_JSON_INDENTATION_SPACES='4'
+  local readonly YQ_EVALUATION_STATEMENT_PATH='.'
+  local readonly DEVCONTAINER_JSON_PATH="$(dirname -- "${1}")/devcontainer.json"
+
+  yq -o=json -I="${YQ_JSON_INDENTATION_SPACES}" "${YQ_EVALUATION_STATEMENT_PATH}" "${1}" >"${DEVCONTAINER_JSON_PATH}"
   return 0
 }
 
 generate() {
+  # TODO: Delete here.
   # START: temporary initial setup
-  local readonly CONFIG_YAML_INPUT_PATH="${SCRIPT_ROOT}/config.yml"
   local readonly PROJECT_PATH="${SCRIPT_ROOT}/test_project"
+  local readonly CONFIG_YAML_INPUT_PATH="${SCRIPT_ROOT}/config.yml"
   # Works like argument
-  set -- "${CONFIG_YAML_INPUT_PATH}" "${PROJECT_PATH}"
+  set -- "${PROJECT_PATH}" "${CONFIG_YAML_INPUT_PATH}"
   # END
 
-  local readonly DEVCONTAINER_DIRNAME='.devcontainer'
+  local readonly DEVCONTAINER_YAML_NAME='devcontainer.yml'
 
-  local readonly devcontainer_directory_path="${2}/${DEVCONTAINER_DIRNAME}"
+  local readonly devcontainer_directory_path="${1}/${DEVCONTAINER_DIRNAME}"
   make_devcontainer_directory_if_not_exists "${devcontainer_directory_path}"
-  local readonly devcontainer_json_output_path="${devcontainer_directory_path}/devcontainer.json"
 
-  generate_devcontainer_json "${1}" "${devcontainer_json_output_path}"
+  evaluate_yaml "${2}" "${devcontainer_directory_path}"
+  convert_devcontainer_yaml_to_json "${devcontainer_directory_path}/${DEVCONTAINER_YAML_NAME}"
 
   return 0
 }
