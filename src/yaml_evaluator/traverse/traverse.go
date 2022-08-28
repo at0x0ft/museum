20f360501da803a1b6f8bcd916b4ae11a7b8239a
at0x0ft <26642966+at0x0ft@users.noreply.github.com>
2022-08-28 07:31:40 +0000

    Add traverse.NodeInfo to ParentNode info.

--- src/yaml_evaluator/traverse/traverse.go
+++ src/yaml_evaluator/traverse/traverse.go
@@ -13,75 +13,76 @@ const (
 
 type NodeInfo struct {
     Node *yaml.Node
+    ParentNode *yaml.Node
     Path string
     Terminal bool
 }
 
 func Traverse(node *yaml.Node, ch chan NodeInfo, order Order) {
-    traverseRecursive(node, "", ch, order)
+    traverseRecursive(node, nil, "", ch, order)
     close(ch)
 }
 
-func sendNodeInfo(ch chan NodeInfo, node *yaml.Node, path string, terminal bool) {
-    ch <- NodeInfo{Node: node, Path: path, Terminal: terminal}
+func sendNodeInfo(ch chan NodeInfo, node *yaml.Node, parentNode *yaml.Node, path string, terminal bool) {
+    ch <- NodeInfo{Node: node, ParentNode: parentNode, Path: path, Terminal: terminal}
 }
 
-func traverseRecursive(node *yaml.Node, path string, ch chan NodeInfo, order Order) {
+func traverseRecursive(node *yaml.Node, parentNode *yaml.Node, path string, ch chan NodeInfo, order Order) {
     switch node.Kind {
     case yaml.MappingNode:
-        traverseMapNode(node, path + ".", ch, order)
+        traverseMapNode(node, parentNode, path + ".", ch, order)
     case yaml.SequenceNode:
-        traverseSequenceNode(node, path, ch, order)
+        traverseSequenceNode(node, parentNode, path, ch, order)
     default:
-        traverseOtherNode(node, path, ch, order)
+        traverseOtherNode(node, parentNode, path, ch, order)
     }
 }
 
-func traverseMapNode(node *yaml.Node, path string, ch chan NodeInfo, order Order) {
+func traverseMapNode(node *yaml.Node, parentNode *yaml.Node, path string, ch chan NodeInfo, order Order) {
     if order == PreOrder {
-        sendNodeInfo(ch, node, path, false)
+        sendNodeInfo(ch, node, parentNode, path, false)
     }
     for index := 0; index < len(node.Content); index += 2 {
         childKeyNode := node.Content[index]
         childValueNode := node.Content[index + 1]
-        visitMapKeyNode(childKeyNode, childValueNode, path + childKeyNode.Value, ch, order)
+        visitMapKeyNode(childKeyNode, childValueNode, node, path + childKeyNode.Value, ch, order)
     }
     if order == PostOrder {
-        sendNodeInfo(ch, node, path, false)
+        sendNodeInfo(ch, node, parentNode, path, false)
     }
 }
 
-func visitMapKeyNode(node *yaml.Node, valueNode *yaml.Node, path string, ch chan NodeInfo, order Order) {
+func visitMapKeyNode(node *yaml.Node, valueNode *yaml.Node, parentNode *yaml.Node, path string, ch chan NodeInfo, order Order) {
     if order == PreOrder {
-        sendNodeInfo(ch, node, path, false)
+        sendNodeInfo(ch, node, parentNode, path, false)
     }
-    traverseRecursive(valueNode, path, ch, order)
+    traverseRecursive(valueNode, node, path, ch, order)
     if order == PostOrder {
-        sendNodeInfo(ch, node, path, false)
+        sendNodeInfo(ch, node, parentNode, path, false)
     }
 }
 
-func traverseSequenceNode(node *yaml.Node, path string, ch chan NodeInfo, order Order) {
+func traverseSequenceNode(node *yaml.Node, parentNode *yaml.Node, path string, ch chan NodeInfo, order Order) {
     if order == PreOrder {
-        sendNodeInfo(ch, node, path, false)
+        sendNodeInfo(ch, node, parentNode, path, false)
     }
     for index, childNode := range node.Content {
         suffix := fmt.Sprintf("[%d]", index)
-        traverseRecursive(childNode, path + suffix, ch, order)
+        traverseRecursive(childNode, node, path + suffix, ch, order)
     }
     if order == PostOrder {
-        sendNodeInfo(ch, node, path, false)
+        sendNodeInfo(ch, node, parentNode, path, false)
     }
 }
 
-func traverseOtherNode(node *yaml.Node, path string, ch chan NodeInfo, order Order) {
+func traverseOtherNode(node *yaml.Node, parentNode *yaml.Node, path string, ch chan NodeInfo, order Order) {
     if order == PreOrder {
-        sendNodeInfo(ch, node, path, len(node.Content) == 0)
+        sendNodeInfo(ch, node, parentNode, path, len(node.Content) == 0)
     }
     for _, childNode := range node.Content {
-        traverseRecursive(childNode, path, ch, order)
+        traverseRecursive(childNode, node, path, ch, order)
     }
     if order == PostOrder {
-        sendNodeInfo(ch, node, path, len(node.Content) == 0)
+        sendNodeInfo(ch, node, parentNode, path, len(node.Content) == 0)
     }
 }
