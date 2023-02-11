3775544761e0e7ddb26bc6c33039c9d7f8ca14b6
at0x0ft <26642966+at0x0ft@users.noreply.github.com>
2023-02-11 06:25:07 +0000

    chore: add restore command descriptions.

--- src/cmd/restore.go
+++ src/cmd/restore.go
@@ -13,15 +13,14 @@ import (
 // restoreCmd represents the restore command
 var restoreCmd = &cobra.Command{
     Use:   "restore",
-    Short: "A brief description of your command",
-    Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+    Short: "Restore config.yml from skeleton.yml.",
+    Long: `restore is a subcommand which generate config.yml from skeleton.yml.
+skeleton.yml is a brief configuration for collections which you want to use as material.
+If you want to generate devcontainer.json & docker-compose.yml from config.yml,
+please run subcommand "deploy" after running this command.`,
     Run: func(cmd *cobra.Command, args []string) {
         fmt.Println("restore called")    // 4debug
+        restore(args)
     },
 }
 
@@ -37,4 +36,9 @@ func init() {
     // Cobra supports local flags which will only run when this command
     // is called directly, e.g.:
     // restoreCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// command body
+func restore(args []string) {
+    fmt.Println(args)   // 4debug
 }
