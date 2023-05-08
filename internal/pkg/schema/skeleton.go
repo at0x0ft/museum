4312207f2a0961c8cfcf32f92e0f2c658a94e34d
at0x0ft <26642966+at0x0ft@users.noreply.github.com>
2023-05-08 23:53:35 +0000

    change: keep path as relative in arguments.docker_compose.files in skeleton.yml.

--- internal/pkg/schema/skeleton.go
+++ internal/pkg/schema/skeleton.go
@@ -287,16 +287,18 @@ func (self *Arguments) getCanonical(baseAbsDir string) (*Arguments, error) {
         return nil, err
     }
 
-    result := *self
-    var dockerComposeAbsPaths []string
-    for _, path := range self.DockerCompose.Files {
-        dockerComposeAbsPaths = append(
-            dockerComposeAbsPaths,
-            resolvePath(baseAbsDir, path),
-        )
-    }
-    result.DockerCompose.Files = dockerComposeAbsPaths
-    return &result, nil
+    // keep given path as relative because paths are passed to devcontainer.json
+    // result := *self
+    // var dockerComposeAbsPaths []string
+    // for _, path := range self.DockerCompose.Files {
+    //     dockerComposeAbsPaths = append(
+    //         dockerComposeAbsPaths,
+    //         resolvePath(baseAbsDir, path),
+    //     )
+    // }
+    // result.DockerCompose.Files = dockerComposeAbsPaths
+    // return &result, nil
+    return self, nil
 }
 
 func (self *Collections) validate() error {
